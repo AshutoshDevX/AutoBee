5fb366ae9384eaca32cf72524b0c89b0be3cf5cb
AshutoshDevX <ashutoshbhagat74@gmail.com>
2025-06-03 13:20:18 +0530

    Add Layout

---
+++ frontend/src/auth/Layout.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { SignInPage } from './SignInPage'
+
+export const Layout = () => {
+    return (
+        <div className="flex justify-center item-center my-20">
+            <SignInPage />
+        </div>
+    )
+}
