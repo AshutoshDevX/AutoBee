2c278602d8a232583a41581ceaf36013f260f3b4
AshutoshDevX <ashutoshbhagat74@gmail.com>
2025-06-04 14:09:02 +0530

    Add signup

---
+++ frontend/src/auth/SignUpPage.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+import { SignUp } from '@clerk/clerk-react'
+export const SignUpPage = () => {
+    return (
+        <div className="flex justify-center item-center my-20"><SignUp /></div>
+    )
+}
