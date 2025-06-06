017f94d7b510ebcd78c9f22eb8ba49d6830d7e54
AshutoshDevX <ashutoshbhagat74@gmail.com>
2025-06-07 03:19:34 +0530

    Add cors and routes

--- backend/server.js
+++ backend/server.js
@@ -1,14 +1,19 @@
 import express from 'express';
-import prisma from './lib/prisma.js'
+import cors from 'cors';
 import dotenv from 'dotenv';
 dotenv.config();
+import sync from './Controllers/sync.js'
 
 const app = express();
 app.use(express.json());
+app.use(cors({
+    origin: "http://localhost:5173",
+    credentials: true
+}))
 
-app.get('/api/users', async (req, res) => {
-    const users = await prisma.user.findMany();
-    res.json(users);
-});
 
-app.listen(3000, () => console.log('Server running at http://localhost:3000'));
+app.use("/api/user", sync)
+
+app.listen(3000, () =>
+    console.log('Server running at http://localhost:3000'
+    ));
