a00d56f4846d04117df028e65900066ed78202a0
AshutoshDevX <ashutoshbhagat74@gmail.com>
2025-06-08 22:04:17 +0530

    update main component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,22 +1,32 @@
-import { Button } from "@/components/ui/button"
-import { useUser } from "@clerk/clerk-react"
-import { Navigate, Outlet } from "react-router"
+import { Route, Routes } from "react-router"
 import { Home } from "./Pages/Home"
 import "./App.css"
 import { useContext } from "react"
 import { AdminContext } from "./components/AdminContext"
-import { useEffect } from "react"
+import { Footer } from './components/Footer.jsx'
+import { Header } from './components/Header.jsx'
+import { SignInPage } from './auth/SignInPage.jsx'
+import { SignUpPage } from './auth/SignUpPage.jsx'
+import { Error } from './Pages/Error.jsx'
+import { Toaster } from 'sonner';
+import { Cars } from './Pages/main/Cars.jsx'
+import { AdminLayout } from './Pages/Admin/AdminLayout.jsx'
 function App() {
-  const [, setIsAdminPage] = useContext(AdminContext);
-
-  useEffect(() => {
-    setIsAdminPage(false);
-  }, [])
+  const [isAdminPage] = useContext(AdminContext);
   return (
     <>
       <div>
-        <Outlet />
-        <Home />
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/signin" element={<SignInPage />} />
+          <Route path="/signup" element={<SignUpPage />} />
+          <Route path="*" element={<Error />} />
+          <Route path="/cars/:id" element={<Cars />} />
+          <Route path="/admin" element={<AdminLayout />} />
+        </Routes>
+        <Toaster richColors />
+        {!isAdminPage && <Footer />}
       </div>
     </>
   )
