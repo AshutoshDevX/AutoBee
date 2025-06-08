cd4fb94011081ea6a33bbabd2c04c83df204a35d
AshutoshDevX <ashutoshbhagat74@gmail.com>
2025-06-08 22:04:26 +0530

    Add AdminPage

---
+++ frontend/src/Pages/Admin/AdminPage.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import { AdminLayout } from './AdminLayout'
+export const AdminPage = () => {
+    return (
+        <AdminLayout>
+            <div className="bg-red-500">AdminPage</div>
+        </AdminLayout>
+    )
+}
