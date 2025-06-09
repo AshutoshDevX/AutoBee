d918348a9711d7dcc414cd5ef94bd43fbc07547d
AshutoshDevX <ashutoshbhagat74@gmail.com>
2025-06-09 20:46:57 +0530

    Add carslist

---
+++ frontend/src/Pages/main/cars/components/CarsList.jsx
@@ -0,0 +1,43 @@
+import React from 'react'
+import { Button } from "@/components/ui/button"
+import { Plus, Search } from 'lucide-react'
+import { Input } from "@/components/ui/input"
+import { Outlet, useNavigate } from 'react-router'
+import { useState } from 'react'
+
+export const CarsList = () => {
+
+    const navigate = useNavigate();
+    const [search, setSearch] = useState("");
+    const handleSearchSubmit = () => { };
+    return (
+        <div className="space-y-4">
+            <div className="flex flex-col sm:flex-row gap-4 items-start sm:items-center justify-between">
+                <Button onClick={() => navigate("create")}
+                    className="flex items-center"
+                >
+                    <Plus className="h-4 w-4" />
+                    Add Car
+                </Button>
+
+
+                <form onSubmit={handleSearchSubmit} className="flex w-full sm:w-auto">
+                    <div className="relative flex-1">
+                        <Search className="absolute left-2.5 top-2.5 h-4 w-4 text-gray-500" />
+                        <Input
+                            type="search"
+                            placeholder="Search cars..."
+                            className="pl-9 w-full sm:w-60"
+                            value={search}
+                            onChange={(e) => setSearch(e.target.value)} />
+                    </div>
+                </form>
+            </div>
+
+
+            {/* Cars Table */}
+        </div>
+
+
+    )
+}
