089f0d5ac20e4cf546f2ce39047981132c014386
AshutoshDevX <ashutoshbhagat74@gmail.com>
2025-06-09 12:45:33 +0530

    Add child routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,18 +1,22 @@
-import { Route, Routes } from "react-router"
+import { Route, Routes, useLocation } from "react-router"
 import { Home } from "./Pages/Home"
 import "./App.css"
-import { useContext } from "react"
-import { AdminContext } from "./components/AdminContext"
+// import { useContext } from "react"
+// import { AdminContext } from "./components/AdminContext"
 import { Footer } from './components/Footer.jsx'
 import { Header } from './components/Header.jsx'
 import { SignInPage } from './auth/SignInPage.jsx'
 import { SignUpPage } from './auth/SignUpPage.jsx'
 import { Error } from './Pages/Error.jsx'
 import { Toaster } from 'sonner';
-import { Cars } from './Pages/main/Cars.jsx'
+import { Cars } from "./Pages/main/Cars/Cars.jsx"
 import { AdminLayout } from './Pages/Admin/AdminLayout.jsx'
+import { AdminPage } from "./Pages/Admin/AdminPage.jsx"
+import { Settings } from "./Pages/main/Settings/Settings.jsx"
+import { TestDrive } from "./Pages/main/test-drive/TestDrive.jsx"
 function App() {
-  const [isAdminPage] = useContext(AdminContext);
+  // const [isAdminPage] = useContext(AdminContext);
+  const { pathname } = useLocation();
   return (
     <>
       <div>
@@ -22,11 +26,15 @@ function App() {
           <Route path="/signin" element={<SignInPage />} />
           <Route path="/signup" element={<SignUpPage />} />
           <Route path="*" element={<Error />} />
-          <Route path="/cars/:id" element={<Cars />} />
-          <Route path="/admin" element={<AdminLayout />} />
+          <Route path="/admin" element={<AdminLayout />}>
+            <Route index element={<AdminPage />} />                {/* /admin */}
+            <Route path="cars" element={<Cars />} />          {/* /admin/cars */}
+            <Route path="settings" element={<Settings />} />  {/* /admin/settings */}
+            <Route path="test-drives" element={<TestDrive />} />
+          </Route>
         </Routes>
         <Toaster richColors />
-        {!isAdminPage && <Footer />}
+        {pathname != "/admin/*" && <Footer />}
       </div>
     </>
   )
