539d861750cdf31a4fde982740f03945e198ff61
AshutoshDevX <ashutoshbhagat74@gmail.com>
2025-06-09 12:46:04 +0530

    Add cars page

---
+++ frontend/src/Pages/main/cars/Cars.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import { useParams } from 'react-router'
+
+
+export const metadata = {
+    title: "Cars | Vehiql",
+    description: "Browse and search for your dream car",
+};
+
+
+export const Cars = () => {
+
+
+
+    return (
+        <div>
+            CarPage
+        </div>
+    )
+}
