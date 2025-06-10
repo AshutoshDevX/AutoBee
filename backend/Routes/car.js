e56d53e7f5ac77b89bee616715af4ab643ba51eb
AshutoshDevX <ashutoshbhagat74@gmail.com>
2025-06-10 22:05:28 +0530

    add processcar route

--- backend/Routes/car.js
+++ backend/Routes/car.js
@@ -1,9 +1,12 @@
 import express from 'express';
 
 import { addCar } from "../Controllers/cars.js";
+import { processCarImageWithAI } from '../Controllers/cars.js';
+
 
 const router = express.Router();
 
 router.post("/addcar", addCar);
+router.post("/processcar", processCarImageWithAI)
 
 export default router;
