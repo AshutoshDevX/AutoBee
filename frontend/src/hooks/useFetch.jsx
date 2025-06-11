fd4afa6bad10cf0e21bd521a2ef6df0507e5aeb9
AshutoshDevX <ashutoshbhagat74@gmail.com>
2025-06-11 21:55:41 +0530

    Add fetchcar and deletecar request

--- frontend/src/hooks/useFetch.jsx
+++ frontend/src/hooks/useFetch.jsx
@@ -1,11 +1,11 @@
 import axios from "axios";
 import { useState } from "react";
 import { toast } from "sonner";
-
 export const useFetch = () => {
     const [data, setData] = useState(undefined);
     const [loading, setLoading] = useState(null);
     const [error, setError] = useState(null);
+
 
     const fn = async ({ userId, carData, images }) => {
         setLoading(true);
@@ -17,6 +17,7 @@ export const useFetch = () => {
                 carData,
                 images
             });
+
 
             setData(response);
             setError(null);
@@ -43,6 +44,69 @@ export const useFetch = () => {
             setLoading(false);
         }
     };
+    const fetchCars = async (userId, search) => {
+        setLoading(true);
+        setError(null);
+        try {
+            const response = await axios.post("http://localhost:3000/api/car", {
+                userId,
+                search
+            });
 
-    return { data, loading, error, fn, fnAi, setData };
+            setData(response.data);
+            setError(null);
+        } catch (error) {
+            setError(error);
+            toast.error(error.message);
+        } finally {
+            setLoading(false);
+        }
+    };
+
+    const updateCarStatusFn = async (id, { featured, status }, userId) => {
+
+
+        setLoading(true);
+        setError(null);
+        try {
+            const response = await axios.put("http://localhost:3000/api/car/updatecar", {
+                userId,
+                featured,
+                status,
+                id
+            });
+
+            setData(response.data);
+            setError(null);
+        } catch (error) {
+            setError(error);
+            toast.error(error.message);
+        } finally {
+            setLoading(false);
+        }
+    };
+
+
+    const deleteCarFn = async (id, userId) => {
+        setLoading(true);
+        setError(null);
+
+        try {
+            const response = await axios.delete("http://localhost:3000/api/car/deletecar", {
+                data: {
+                    userId,
+                    id,
+                },
+            });
+
+            setData(response.data);
+            setError(null);
+        } catch (error) {
+            setError(error);
+            toast.error(error.message);
+        } finally {
+            setLoading(false);
+        }
+    };
+    return { data, loading, error, fn, fnAi, fetchCars, updateCarStatusFn, deleteCarFn };
 };
