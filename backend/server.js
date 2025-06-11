fd2a2e2543b95adffd1187de4a795cc18cf3e1a4
AshutoshDevX <ashutoshbhagat74@gmail.com>
2025-06-11 14:37:39 +0530

    minor updates

--- backend/server.js
+++ backend/server.js
@@ -7,11 +7,15 @@ import admin from './Routes/admin.js'
 import car from './Routes/car.js'
 import cookieParser from 'cookie-parser';
 const app = express();
-app.use(express.json());
+
 app.use(cors({
     origin: "http://localhost:5173",
     credentials: true
 }))
+
+
+app.use(express.json({ limit: "10mb" }));
+app.use(express.urlencoded({ extended: true, limit: "10mb" }));
 
 app.use(cookieParser());
 app.use("/api/user", sync);
