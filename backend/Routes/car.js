8bf22700137c0b8a3cbcc7b343e3efbf5c87c675
AshutoshDevX <ashutoshbhagat74@gmail.com>
2025-06-11 21:56:02 +0530

    add delete update get routes

--- backend/Routes/car.js
+++ backend/Routes/car.js
@@ -1,12 +1,14 @@
 import express from 'express';
-
-import { addCar } from "../Controllers/cars.js";
+import { addCar, deleteCar, getCars, updateCarStatus } from "../Controllers/cars.js";
 import { processCarImageWithAI } from '../Controllers/cars.js';
 
 
 const router = express.Router();
 
 router.post("/addcar", addCar);
-router.post("/processcar", processCarImageWithAI)
+router.post("/processcar", processCarImageWithAI);
+router.delete("/deletecar", deleteCar)
+router.post("/", getCars);
+router.put("/updatecar", updateCarStatus);
 
 export default router;
