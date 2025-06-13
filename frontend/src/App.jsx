409d23e6195ec0c38f55d132d3c78873c480c86a
AshutoshDevX <ashutoshbhagat74@gmail.com>
2025-06-14 03:17:16 +0530

    minor updates

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -13,6 +13,8 @@ import { AdminPage } from "./Pages/Admin/AdminPage.jsx"
 import { Settings } from "./Pages/Admin/settings/Settings.jsx"
 import { TestDrive } from "./Pages/Admin/test-drive/TestDrive.jsx"
 import { AddCarPage } from "./Pages/Admin/cars/components/AddCarPage.jsx"
+import { CarsPage } from "./Pages/main/Cars/Cars.jsx"
+
 function App() {
   const { pathname } = useLocation();
   return (
@@ -31,6 +33,9 @@ function App() {
             <Route path="create" element={<AddCarPage />} />
             <Route path="settings" element={<Settings />} />
             <Route path="test-drives" element={<TestDrive />} />
+          </Route>
+          <Route path="/cars" element={<CarsPage />}>
+
           </Route>
         </Routes>
         <Toaster richColors />
