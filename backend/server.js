cf69a65ad318e48791d2d1c413a9b295298ccc46
AshutoshDevX <ashutoshbhagat74@gmail.com>
2025-06-14 02:34:47 +0530

    minor updates

--- backend/server.js
+++ backend/server.js
@@ -7,6 +7,8 @@ import admin from './Routes/admin.js'
 import car from './Routes/car.js'
 import cookieParser from 'cookie-parser';
 import settings from './Routes/settings.js';
+import home from './Routes/home.js';
+
 const app = express();
 
 app.use(cors({
@@ -23,7 +25,7 @@ app.use("/api/user", sync);
 app.use("/api/admin", admin);
 app.use("/api/car", car);
 app.use("/api/settings", settings);
-
+app.use("/api", home);
 
 app.listen(3000, () =>
     console.log('Server running at http://localhost:3000'
