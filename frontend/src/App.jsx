6d806ffb49bbd73e5ef28e25a6fd476952e381d1
AshutoshDevX <ashutoshbhagat74@gmail.com>
2025-06-16 23:02:27 +0530

    Add routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -9,12 +9,15 @@ import { Error } from './Pages/Error.jsx'
 import { Toaster } from 'sonner';
 import { Cars } from "./Pages/Admin/cars/Cars.jsx"
 import { AdminLayout } from './Pages/Admin/AdminLayout.jsx'
-import { AdminPage } from "./Pages/Admin/AdminPage.jsx"
+import AdminPage from "./Pages/Admin/AdminPage.jsx"
 import { Settings } from "./Pages/Admin/settings/Settings.jsx"
-import { TestDrive } from "./Pages/Admin/test-drive/TestDrive.jsx"
+import TestDrives from "./Pages/Admin/testdrive/TestDrives.jsx"
 import { AddCarPage } from "./Pages/Admin/cars/components/AddCarPage.jsx"
 import { CarsPage } from "./Pages/main/Cars/Cars.jsx"
-
+import SavedCars from "./Pages/main/savedcars/SavedCars.jsx"
+import { CarDetailsPage } from "./Pages/main/Cars/cardetails/CarsPage.jsx"
+import TestDrivePage from "./Pages/main/testdrive/TestDrive.jsx"
+import Reservations from "./Pages/main/reservations/Reservation.jsx"
 function App() {
   const { pathname } = useLocation();
   return (
@@ -32,11 +35,13 @@ function App() {
             <Route path="cars/create" element={<AddCarPage />} />
             <Route path="create" element={<AddCarPage />} />
             <Route path="settings" element={<Settings />} />
-            <Route path="test-drives" element={<TestDrive />} />
+            <Route path="testdrives" element={<TestDrives />} />
           </Route>
-          <Route path="/cars" element={<CarsPage />}>
-
-          </Route>
+          <Route path="/cars" element={<CarsPage />} />
+          <Route path="/cars/:id" element={<CarDetailsPage />} />
+          <Route path="/savedcars" element={<SavedCars />} />
+          <Route path="testdrive/:id" element={<TestDrivePage />} />
+          <Route path="/reservations" element={<Reservations />} />
         </Routes>
         <Toaster richColors />
         <Footer />
