b118a676e5321a172c0c883769696e278b385d28
AshutoshDevX <ashutoshbhagat74@gmail.com>
2025-06-16 23:05:15 +0530

    updates useFetch

--- frontend/src/hooks/useFetch.jsx
+++ frontend/src/hooks/useFetch.jsx
@@ -206,8 +206,147 @@ export const useFetch = () => {
         }
     }
 
+    const getCars = async (...args) => {
+        setLoading(true);
+        setError(null);
+        try {
+            const response = await axios.post("http://localhost:3000/api/cars", args[0]);
+            setData(response.data);
+            setError(null);
+        } catch (error) {
+            setError(error);
+            console.log(error)
+            toast.error(error.message);
+        } finally {
+            setLoading(false);
+        }
+    }
+
+    const toggleSavedCarFn = async (carId, userId) => {
+        setLoading(true);
+        setError(null);
+        try {
+            const response = await axios.get("http://localhost:3000/api/togglesavedcar", {
+                params: {
+                    carId,
+                    userId
+                }
+            });
+
+            setData(response.data);
+            setError(null);
+        } catch (error) {
+            setError(error);
+            console.log(error)
+            toast.error(error.message);
+        } finally {
+            setLoading(false);
+        }
+    }
+
+    const bookTestDriveFn = async (args) => {
+        setLoading(true);
+        setError(null);
+        try {
+            const response = await axios.post("http://localhost:3000/api/testdrive", args);
+
+            setData(response.data);
+            setError(null);
+        } catch (error) {
+            setError(error);
+            console.log(error)
+            toast.error(error.message);
+        } finally {
+            setLoading(false);
+        }
+    }
+
+    const cancelBookingFn = async (bookingId, userId) => {
+
+        setLoading(true);
+        setError(null);
+        try {
+            const response = await axios.delete("http://localhost:3000/api/canceltestdrive", {
+                params: {
+                    bookingId,
+                    userId
+                }
+            });
+            setData(response.data);
+            setError(null);
+        } catch (error) {
+            setError(error);
+            console.log(error)
+            toast.error(error.message);
+        } finally {
+            setLoading(false);
+        }
+    }
+
+    const fetchTestDrives = async (args) => {
+
+        setLoading(true);
+        setError(null);
+        try {
+            const response = await axios.post("http://localhost:3000/api/admin/admintestdrives", args);
+
+            setData(response.data);
+
+            setError(null);
+        } catch (error) {
+            setError(error);
+            console.log(error)
+            toast.error(error.message);
+        } finally {
+            setLoading(false);
+        }
+    }
+    const updateStatusFn = async (args) => {
+
+
+        setLoading(true);
+        setError(null);
+        try {
+            const response = await axios.put("http://localhost:3000/api/admin/updatestatus", args);
+
+            setData(response.data);
+
+            setError(null);
+        } catch (error) {
+            setError(error);
+            console.log(error)
+            toast.error(error.message);
+        } finally {
+            setLoading(false);
+        }
+    }
+
+    const cancelTestDriveFn = async (bookingId, userId) => {
+
+
+        setLoading(true);
+        setError(null);
+        try {
+            const response = await axios.delete("http://localhost:3000/api/admin/canceltestdrive", {
+                params: {
+                    bookingId,
+                    userId
+                }
+            });
+
+            setData(response.data);
+
+            setError(null);
+        } catch (error) {
+            setError(error);
+            console.log(error)
+            toast.error(error.message);
+        } finally {
+            setLoading(false);
+        }
+    }
     return {
         data, loading, error, fn, fnAi, fetchCars, updateCarStatusFn, deleteCarFn, fetchDealershipInfo, fetchUsers, saveHours,
-        updateRole, processImageFn
+        updateRole, processImageFn, getCars, toggleSavedCarFn, bookTestDriveFn, cancelBookingFn, fetchTestDrives, updateStatusFn, cancelTestDriveFn
     };
 };
