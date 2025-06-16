b39f84504fd5f2d7b9b86e7ca5a3e04b59604a0a
AshutoshDevX <ashutoshbhagat74@gmail.com>
2025-06-16 23:03:52 +0530

    Add admin routes

--- backend/Routes/admin.js
+++ backend/Routes/admin.js
@@ -1,8 +1,12 @@
 import express from 'express';
-import { getAdmin } from '../Controllers/admin.js';
-
+import { getAdmin, getDashboardData, updateTestDriveStatus } from '../Controllers/admin.js';
+import { getAdminTestDrives } from '../Controllers/admin.js';
+import { cancelTestDrive } from '../Controllers/testdrive.js';
 const router = express.Router();
 
 router.get("/:userId", getAdmin);
-
+router.post("/admintestdrives", getAdminTestDrives);
+router.put("/updatestatus", updateTestDriveStatus);
+router.delete("/canceltestdrive", cancelTestDrive);
+router.get("/dashboarddata/:userId", getDashboardData);
 export default router;
