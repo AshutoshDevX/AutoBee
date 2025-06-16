3ca0f58ba19ccd3445ac544efcc3b6ebde5830df
AshutoshDevX <ashutoshbhagat74@gmail.com>
2025-06-17 02:02:26 +0530

    minor updates

--- backend/server.js
+++ backend/server.js
@@ -12,11 +12,8 @@ import carlisting from "./Routes/carlisting.js"
 import testdrive from "./Routes/testdrive.js"
 
 const app = express();
-
-app.use(cors({
-    origin: "http://localhost:5173",
-    credentials: true
-}))
+const port = process.env.PORT || 3000;
+app.use(cors())
 
 
 app.use(express.json({ limit: "10mb" }));
@@ -32,7 +29,7 @@ app.use("/api", carlisting);
 app.use("/api", testdrive);
 
 
-app.listen(3000, () =>
+app.listen(port, () =>
     console.log('Server running at http://localhost:3000'
     ));
 
